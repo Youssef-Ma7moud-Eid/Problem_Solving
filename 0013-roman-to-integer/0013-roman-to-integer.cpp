b6e89e9bfdb64f0096d2b6b4dd41e9314861ff80
Youssef Mahmoud Eid <ym1977@fayoum.edu.eg>
2025-07-10 16:00:21 +0300

    Time: 4 ms (65.76%), Space: 13.3 MB (36.13%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int sum=0;
+        map<char,int>m; 
+         m['I']=1;
+         m['V']=5;
+          m['X']=10;
+           m['L']=50; m['C']=100; m['D']=500; m['M']=1000;
+        
+      if(s.size()==1)
+      {
+        return m[s[0]];
+      }
+     
+      sum+=m[s[0]];
+        for(int i=1;i<s.size();i++)
+        {
+         if(m[s[i]]>m[s[i-1]])
+         {
+            sum-=m[s[i-1]];
+          sum+=(m[s[i]]-m[s[i-1]]);
+         }
+         else{
+            sum+=m[s[i]];
+           
+         }
+        }
+       
+        return sum;
+    }
+};
