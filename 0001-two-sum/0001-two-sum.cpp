5721f13f689c52b0238a377a76ff1b4e7fbb386d
Youssef Mahmoud Eid <ym1977@fayoum.edu.eg>
2025-07-11 20:50:29 +0300

    Time: 16 ms (40.84%), Space: 18.4 MB (6.92%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>array(2);
+        map<int,int>map;
+        for(int i=0;i<nums.size();i++)
+        {
+             if(map[target-nums[i]]>=1)
+            {
+                array[0]=i;
+                array[1]=map[target-nums[i]]-1;
+            }
+            map[nums[i]]=i+1;
+
+           
+        }
+        return array;
+    }
+};
