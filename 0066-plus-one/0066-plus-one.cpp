ac70735d80648536a87df04dc36083f20f0d7b23
Youssef Mahmoud Eid <ym1977@fayoum.edu.eg>
2025-07-11 22:37:04 +0300

    Time: 0 ms (100%), Space: 11.4 MB (88.27%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int size = digits.size() - 1;
+
+ if (digits[size] <= 8)
+ {
+     digits[size]++;
+     return digits;
+ }
+ else {
+     bool flag = true;
+     for (int i = size; i >= 0; i--)
+     {
+         if (digits[i] <= 8)
+         {
+             digits[i]++;
+             flag = false;
+             break;
+         }
+         else {
+             digits[i] = 0;
+         }
+     }
+     if (flag)
+     {
+         vector<int>digits2(size + 2);
+         digits2[0] = 1;
+         for (int i = 0; i <= size; i++)
+         {
+             digits2[i + 1] = digits[i];
+         }
+         
+         return digits2;
+        
+     }
+     else {
+         return digits;
+        
+     }
+    
+ }
+    }
+
+       
+    
+};
