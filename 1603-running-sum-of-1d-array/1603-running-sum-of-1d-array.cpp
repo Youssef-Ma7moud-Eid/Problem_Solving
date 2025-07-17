3a2cd8ad4d4e022ff2ed881f7f938f90fc312e97
Youssef Mahmoud Eid <ym1977@fayoum.edu.eg>
2025-07-17 20:40:10 +0300

    Time: 0 ms (100%), Space: 12.5 MB (49.43%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums)
+    {
+        for(int i=0;i<nums.size();++i)
+        {
+          if(i>=1)
+          {
+            nums[i]+=nums[i-1];
+          }
+        }
+        return nums;
+    }
+
+};
