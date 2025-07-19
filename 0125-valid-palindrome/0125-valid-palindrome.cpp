d14a240bd20200b477581e5a1166d1cfac2bdbdf
Youssef Mahmoud Eid <ym1977@fayoum.edu.eg>
2025-07-19 20:16:17 +0300

    Time: 4 ms (17.11%), Space: 10.2 MB (36.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -5,7 +5,10 @@ public:
         string ss;
       for(int i=0;i<s.size();i++)
       {
-         s[i]=tolower(s[i]);
+         if(int(s[i])>=65 &&int(s[i])<=90)
+         {
+            s[i]+=32;
+         }
          if((int(s[i])>= 97 && int(s[i]) <=122)||(int(s[i])>= 48 && int(s[i]) <= 57))
          {
             ss.push_back(s[i]);
