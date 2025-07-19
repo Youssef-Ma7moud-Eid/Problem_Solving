0d922dd1ced53a59e46b02ad4aa0770fd35deab3
Youssef Mahmoud Eid <ym1977@fayoum.edu.eg>
2025-07-19 19:25:23 +0300

    Time: 0 ms (100%), Space: 27.4 MB (16.84%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,9 +3,8 @@ public:
     void reverseString(vector<char>& s) {
         for(int i=0;i<s.size()/2;i++)
         {
-            char ss=s[i];
-            s[i]=s[s.size()-i-1];
-            s[s.size()-i-1]=ss;
+           swap(s[i],s[s.size()-i-1]);
+           
         }
     }
 };
