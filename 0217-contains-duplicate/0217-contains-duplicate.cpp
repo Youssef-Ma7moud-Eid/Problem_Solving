de1f8e87ead568ac160def16ab4e90f6aec0483d
Youssef Mahmoud Eid <ym1977@fayoum.edu.eg>
2025-07-19 19:16:37 +0300

    Time: 25 ms (82.78%), Space: 71 MB (88.68%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums)
     {
-       
+       int size=nums.size();
        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++)
+        for(int i=1;i<size;i++)
         {
            
             if(nums[i]==nums[i-1])
