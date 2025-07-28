82a423099bbaec4e8a38978234e27cc048f9f5f7
Youssef Mahmoud Eid <ym1977@fayoum.edu.eg>
2025-07-28 16:02:43 +0300

    Time: 0 ms (100%), Space: 8.9 MB (31.82%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) 
+    {
+        int sum1=0,sum2=0;
+        bool flag=true;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==' '&&flag==true)
+            {
+                sum1 = sum2;
+                flag = false;
+                sum2=0;
+            }
+            else if(s[i]!=' '){
+                sum2++;
+                flag=true;
+            }
+        }
+        if(sum2>0)
+        {
+            sum1=sum2;
+        }
+        return sum1;
+    }
+};
